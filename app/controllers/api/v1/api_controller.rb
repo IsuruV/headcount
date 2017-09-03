9b428e0b6e22042eab9fffb6af55adf82b41248b
IsuruV <isuru260@gmail.com>
2017-09-03 20:46:54 +0000

    created endpoint for fetching count of users in closest area

---
+++ app/controllers/api/v1/api_controller.rb
@@ -0,0 +1,10 @@
+module Api::V1
+ class ApiController < ApplicationController
+      # include DeviseTokenAuth::Concerns::SetUserByToken
+      # # Prevent CSRF attacks by raising an exception.
+      # # For APIs, you may want to use :null_session instead.
+      # # protect_from_forgery with: :exception
+      # protect_from_forgery with: :null_session
+      # before_filter :authenticate_user!
+  end
+end
--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,2 +1,5 @@
 module LocationsHelper
+    def format_params(params)
+        [params[:lat].to_f, params[:lng].to_f]
+    end
 end
